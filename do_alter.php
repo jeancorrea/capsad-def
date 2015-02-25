3bd7403d2abb2feb782d6ced3667d34f5f3e7864
Jean Correa <jean.do.violao@gmail.com>
2015-02-25 15:58:06 -0300

    Alter 2

--- do_alter.php
+++ do_alter.php
@@ -2,14 +2,16 @@
 
 <?php
 
-$nome = $_POST['nome'];
-$id = $_POST['id'];
-$novonome = $_POST['novonome'];
-$novoid = $_POST['novoid'];
+$antnome = $_POST['antnome'];
+$antid = $_POST['antid'];
 
-$cod1 = mysql_query("UPDATE pacientes SET id = '$novoid' AND nome = '$novonome' WHERE id = '$id' AND nome = '$nome'");
+$cod1 = mysql_query("UPDATE pacientes SET id = '$id', nome = '$nome' WHERE id = '$antid' AND nome = '$antnome'");
 
-$cod2 = mysql_query("UPDATE atendimentos SET")
+$cod2 = mysql_query("UPDATE atendimentos SET paciente = '$nome-$id' WHERE paciente = '$antnome-$antid'");
+
+if ($cod1 == true && $cod2 == true) {
+	echo "Dados atualizados com sucesso!";
+}
 
 
 ?>
--- mid_alter.php
+++ mid_alter.php
@@ -4,27 +4,111 @@
 
 $paciente = $_POST['paciente'];
 $array = explode("-", $paciente);
-$nome = $array[0];
-$id = $array[1];
+$antnome = $array[0];
+$antid = $array[1];
 
 ?>
 
 <form action="do_alter.php" method="POST" id="mid_alter">
 	<fieldset>
 		<label>Nome</label>
-		<input type="text" name="nome" value="<?php echo $nome; ?>"><br />
+		<input type="text" name="antnome" value="<?php echo $antnome; ?>"><br />
 
 		<label>Prontuário</label>
-		<input type="text" name="id" value="<?php echo $id; ?>"><br />
+		<input type="text" name="antid" value="<?php echo $antid; ?>"><br />
 	</fieldset>
 
 	<fieldset>
-		<label>Novo nome</label>
-		<input type="text" name="novonome" value="<?php echo $nome; ?>"><br />
+		<legend>Identificação</legend>
+			<label>Nome do paciente</label>
+			<input type="text" name="nome" autofocus><br />
 
-		<label>Novo prontuário</label>
-		<input type="text" name="novoid" value="<?php echo $id; ?>"><br />
-	</fieldset>
+			<label>Data de nascimento</label>
+			<input type="text" name="dn_d" class="data"> / <input type="text" name="dn_m" class="data"> / <input type="text" name="dn_a" class="data"><br />
+
+			<label>Gênero</label>
+			<select name="genero">
+				<option></option>
+				<option>Masculino</option>
+				<option>Feminino</option>
+			</select><!--genero--><br />
+
+			<label>Endereço</label>
+			<input type="text" name="endereco" placeholder="Rua, avenida etc"><input type="text" name="cep" placeholder="CEP"><input type="text" name="cidade" placeholder="Cidade"><br />
+
+			<label>Naturalidade</label>
+			<input type="text" name="naturalidade"><br />
+
+			<label>Escolaridade</label>
+			<select name="escola">
+				<option></option>
+				<option>Analfabeto</option>
+				<option>Ensino Fundamental incompleto</option>
+				<option>Ensino Fundamental completo</option>
+				<option>Ensino Médio incompleto</option>
+				<option>Ensino Médio completo</option>
+				<option>Ensino Superior incompleto</option>
+				<option>Ensino Superior completo</option>
+				<option>Curso Normal Superior</option>
+				<option>Pós-graduação</option>
+				<option>Mestrado</option>
+				<option>Doutorado</option>
+			</select><!--escola--><br />
+
+			<label>Telefones</label>
+			<input type="text" name="telefones"><br />
+		</fieldset>
+
+		<fieldset>
+		<legend>Filiação</legend>
+			<label>Pai</label>
+			<input type="text" name="pai"><br />
+
+			<label>Mãe</label>
+			<input type="text" name="mae"><br />
+		</fieldset>
+
+		<fieldset>
+		<legend>Documentação</legend>
+			<label>Identidade</label>
+			<input type="text" name="rg" placeholder="Número"><input type="text" name="orgaorg" placeholder="Órgão expedidor"><input type="text" name="de_d" class="data" placeholder="Data"> / <input type="text" name="de_m" class="data" placeholder="de"> / <input type="text" class="data" name="de_a" placeholder="emissão"><br />
+
+			<label>CPF</label>
+			<input type="text" name="cpf"><br />
+
+			<label>Cartão SUS</label>
+			<input type="text" name="cns"><br />
+
+			<label>Certidão</label>
+			<input type="text" name="certidao"><br />
+		</fieldset>
+
+		<fieldset>
+		<legend>Informações adicionais</legend>
+			<label>Demanda</label>
+			<select name="demanda">
+				<option></option>
+				<option>Adolescente</option>
+				<option>DQ</option>
+				<option>Familiar</option>
+			</select><!--demanda--><br />
+
+			<label>Substâncias</label>
+			<textarea name="substancias" placeholder="Separe os nomes das substâncias por vírgulas"></textarea><br />
+
+			<label>CIDs</label>
+			<input type="text" name="cidp" placeholder="Primário"><input type="text" name="cids" placeholder="Secundário"><br />
+
+			<label>Status</label>
+			<select name="status">
+				<option></option>
+				<option>Arquivo corrente</option>
+				<option>Arquivo morto</option>
+			</select><!--status--><br />
+
+			<label>Início do tratamento</label>
+			<input type="text" name="di_d" class="data"> / <input type="text" name="di_m" class="data"> / <input type="text" name="di_a" class="data"><br />
+		</fieldset>
 
 	<fieldset class="buttons">
 		<input type="submit" class="button" value="Enviar">&nbsp;<input type="reset" class="button" value="Limpar">
