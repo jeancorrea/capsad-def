fa7d93fe714f3f591ec8a6009bb8cfb87c90d73a
Jean Correa <jean.do.violao@gmail.com>
2015-05-27 16:21:06 -0300

    Corrigido links do menu

--- sidebar.php
+++ sidebar.php
@@ -11,9 +11,9 @@
 		<li><a href="consulta.php">Consultar procedimentos</a></li>
 	</ul><!--menu-->
 	<ul class="menu"><h3>Links úteis</h3>
-		<li><a href="http://localhost/phpmyadmin" target="_BLANK">phpMyAdmin</a></li>
-		<li><a href="http://sistemas.macae.rj.gov.br:82/apps/oficiodigital/default.asp" target="_BLANK">Ofício Digital</a></li>
-		<li><a href="https://cadastro.saude.gov.br/cadsusweb/login.jsp" target="_BLANK">CadWeb DataSUS</li>
-		<li><a href="http://www.receita.fazenda.gov.br/Aplicacoes/ATCTA/CPF/ConsultaPublica.asp" target="_BLANK">Consulta CPF</a></li>
+		<li><a href="http://localhost/phpmyadmin" target="_blank">phpMyAdmin</a></li>
+		<li><a href="http://sistemas.macae.rj.gov.br:82/apps/oficiodigital/default.asp" target="_blank">Ofício Digital</a></li>
+		<li><a href="https://cadastro.saude.gov.br/cadsusweb/login.jsp" target="_blank">CadWeb DataSUS</li>
+		<li><a href="http://www.receita.fazenda.gov.br/Aplicacoes/ATCTA/CPF/ConsultaPublica.asp" target="_blank">Consulta CPF</a></li>
 	</ul>
 </div><!--menubar-->
